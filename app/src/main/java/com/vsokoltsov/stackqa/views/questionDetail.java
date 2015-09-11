fdd02fb6704a48c56e945e76cd7b085ee21be174
vsokoltsov <vforvad@gmail.com>
2015-09-11 14:36:41 +0300

    change some view settings

--- app/src/main/java/com/vsokoltsov/stackqa/views/questionDetail.java
+++ app/src/main/java/com/vsokoltsov/stackqa/views/questionDetail.java
@@ -1,5 +1,6 @@
 package com.vsokoltsov.stackqa.views;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
