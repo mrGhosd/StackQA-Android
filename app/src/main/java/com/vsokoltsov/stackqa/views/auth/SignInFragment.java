dbdf18fdcf17382e1a29cf305bea82d1caa97d78
vsokoltsov <vforvad@gmail.com>
2015-11-16 01:06:13 +0300

    Add handling of auth information

--- app/src/main/java/com/vsokoltsov/stackqa/views/auth/SignInFragment.java
+++ app/src/main/java/com/vsokoltsov/stackqa/views/auth/SignInFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.vsokoltsov.stackqa.R;
@@ -26,6 +28,13 @@ public class SignInFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragmentView =  inflater.inflate(R.layout.sign_in, container, false);
+        Button signInButton = (Button) fragmentView.findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(v);
+            }
+        });
         return fragmentView;
     }
 
@@ -40,5 +49,13 @@ public class SignInFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    public void signIn(View v){
+        EditText email = (EditText) getView().findViewById(R.id.emailField);
+        EditText password = (EditText) getView().findViewById(R.id.passwordField);
+
+        String emailString = email.getText().toString();
+        String passwordString = password.getText().toString();
     }
 }
