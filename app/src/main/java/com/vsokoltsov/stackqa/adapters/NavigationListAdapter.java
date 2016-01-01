eba101b4d9bacddad717dd6da76d416129d6fb39
vsokoltsov <vforvad@gmail.com>
2016-01-01 18:22:35 +0300

    add some kind of material design navigation header menu

--- app/src/main/java/com/vsokoltsov/stackqa/adapters/NavigationListAdapter.java
+++ app/src/main/java/com/vsokoltsov/stackqa/adapters/NavigationListAdapter.java
@@ -6,6 +6,9 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,9 +22,13 @@ import com.android.volley.toolbox.ImageRequest;
 import com.vsokoltsov.stackqa.R;
 import com.vsokoltsov.stackqa.controllers.AppController;
 import com.vsokoltsov.stackqa.models.Answer;
+import com.vsokoltsov.stackqa.models.AuthManager;
 import com.vsokoltsov.stackqa.models.NavigationItem;
+import com.vsokoltsov.stackqa.models.User;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by vsokoltsov on 01.10.15.
@@ -30,6 +37,7 @@ public class NavigationListAdapter extends BaseAdapter {
     private Activity activity;
     private LayoutInflater inflater;
     private List<NavigationItem> navigationList;
+    private NavigationItem navigation;
 
     public NavigationListAdapter(Activity activity, List<NavigationItem> navigationList) {
         this.activity = activity;
@@ -52,43 +60,20 @@ public class NavigationListAdapter extends BaseAdapter {
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
+        navigation = navigationList.get(position);
 
         if (inflater == null)
             inflater = (LayoutInflater) activity
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (convertView == null)
-            convertView = inflater.inflate(R.layout.navigation_item, null);
-
-        //Перечисляем элементы в layout-е строки списка вопросов
-        final TextView text = (TextView) convertView.findViewById(R.id.navigationText);
-        final ImageView image = (ImageView) convertView.findViewById(R.id.navigationImage);
-
-        // getting movie data for the row
-        final NavigationItem navigation = navigationList.get(position);
-
-
-        if (navigation.isUserCell()){
-
-            String url = AppController.APP_HOST + navigation.getUser().getAvatarUrl();
-            ImageRequest ir = new ImageRequest(url, new Response.Listener<Bitmap>() {
-                @Override
-                public void onResponse(Bitmap response) {
-                    int avatarSize = (int) activity.getResources().getDimension(R.dimen.user_profile_avatar_in_navigation_menu);
-                    int avatarMargin = (int) activity.getResources().getDimension(R.dimen.user_profile_avatar_top_margin);
-                    text.setText(navigation.getUser().getCorrectNaming());
-                    text.setTextSize(16);
-                    setMarginsForTextField(text, avatarMargin);
-                    image.setImageBitmap(getRoundedShape(response));
-                    image.getLayoutParams().height = avatarSize;
-                    image.getLayoutParams().width = avatarSize;
-                }
-            }, 0, 0, null, null);
-            AppController.getInstance().addToRequestQueue(ir);
-        }
-        else {
-            text.setText(navigation.getTitle());
-            image.setImageResource(navigation.getImage());
-        }
+            if (navigation.isUserCell()) {
+                convertView = inflater.inflate(R.layout.navigation_header, null);
+                configViewForUserItem(convertView);
+            }
+            else {
+                convertView = inflater.inflate(R.layout.navigation_item, null);
+                configViewForSimilarItem(convertView);
+            }
         return convertView;
     }
 
@@ -119,4 +104,50 @@ public class NavigationListAdapter extends BaseAdapter {
         params.setMargins(15, margin, 0, 0);
         textview.setLayoutParams(params);
     }
+
+    private void configViewForUserItem(View contentView) {
+        User user = navigation.getUser();
+        final CircleImageView avatarView = (CircleImageView) contentView.findViewById(R.id.circleView);
+        TextView userName = (TextView) contentView.findViewById(R.id.name);
+        TextView userEmail = (TextView) contentView.findViewById(R.id.email);
+        if (user.getCorrectNaming().equals(user.getEmail())) {
+            userName.setText(user.getCorrectNaming());
+        }
+        else {
+            userName.setText(user.getCorrectNaming());
+            userName.setText(user.getEmail());
+        }
+        Drawable background = (Drawable) activity.getResources().getDrawable(R.drawable.backgroundploy);
+        int width = background.getIntrinsicWidth();
+        Bitmap bitmap = ((BitmapDrawable) background).getBitmap();
+        int backgroundHeight = (int) activity.getResources().getDimension(R.dimen.user_header_navigation_menu_height);
+        Drawable d = new BitmapDrawable(activity.getResources(), Bitmap.createScaledBitmap(bitmap, width, backgroundHeight, true));
+        contentView.findViewById(R.id.backgroundView).setBackground(d);
+        String url = AppController.APP_HOST + navigation.getUser().getAvatarUrl();
+        ImageRequest ir = new ImageRequest(url, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                int avatarSize = (int) activity.getResources().getDimension(R.dimen.user_profile_avatar_in_navigation_menu);
+                int avatarMargin = (int) activity.getResources().getDimension(R.dimen.user_profile_avatar_top_margin);
+                avatarView.setImageBitmap(response);
+//                text.setText(navigation.getUser().getCorrectNaming());
+//                text.setTextSize(16);
+//                setMarginsForTextField(text, avatarMargin);
+//                image.setImageBitmap(getRoundedShape(response));
+//                image.getLayoutParams().height = avatarSize;
+//                image.getLayoutParams().width = avatarSize;
+            }
+        }, 0, 0, null, null);
+        AppController.getInstance().addToRequestQueue(ir);
+    }
+
+    private void configViewForSimilarItem(View convertView) {
+        final TextView text = (TextView) convertView.findViewById(R.id.navigationText);
+        final ImageView image = (ImageView) convertView.findViewById(R.id.navigationImage);
+
+        text.setText(navigation.getTitle());
+        image.setImageResource(navigation.getImage());
+    }
 }
+
+
