1359d8d6387f472c3188503b4091e16a1dde5aac
vsokoltsov <vforvad@gmail.com>
2016-02-11 21:29:20 +0300

    finally, implement fixed answer list inside view pager

--- app/src/main/java/com/vsokoltsov/stackqa/adapters/QuestionDetailViewPager.java
+++ app/src/main/java/com/vsokoltsov/stackqa/adapters/QuestionDetailViewPager.java
@@ -20,33 +20,17 @@ public class QuestionDetailViewPager extends ViewPager {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-//
-//        boolean wrapHeight = MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.AT_MOST;
-//
-//        final View tab = getChildAt(0);
-//        int width = getMeasuredWidth();
-//        int tabHeight = tab.getMeasuredHeight();
-//
-//        if (wrapHeight) {
-//            // Keep the current measured width.
-//            widthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
-//        }
-//
-//        int fragmentHeight = measureFragment(((Fragment) getAdapter().instantiateItem(this, getCurrentItem())).getView());
-//        heightMeasureSpec = MeasureSpec.makeMeasureSpec(fragmentHeight +
-//                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-//                50,
-//                getResources().getDisplayMetrics()), MeasureSpec.AT_MOST);
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
 
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
-    public int measureFragment(View view) {
-        if (view == null)
-            return 0;
-
-        view.measure(0, 0);
-        return view.getMeasuredHeight();
-    }
 }
